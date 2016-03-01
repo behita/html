2056dd1440830b57d2bec5aa9b73c1dd108f6437
behita <behatriz.13gnicasio@gmail.com>
2016-02-29 22:10:08 -0500

    examen

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>nicasio</title>
+	</head>
+	<body>
+	<marquee>
+	<h1>beatriz gargate nicasio</h1></marquee>
+	<img src="1.gif" width="" alt="">
+	<img src="2.gif" width="" alt="">
+	<marquee> 
+	<<h1> beatriz gargate nicasio</h1></marquee>
+	</body>
+</html>
